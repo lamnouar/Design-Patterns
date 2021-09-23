cb6076f0a838b471f3bbbfe92149bbd093b10e68
abdelhak lamnouar <lamnouar.abdelhak@gmail.com>
2021-09-22 21:18:32 -0400

    mediator pattern

---
+++ behavioral-patterns/Mediator/Mediator/Colleague.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Mediator
+{
+    public abstract class Colleague
+    {
+        protected Mediator mediator;
+        // Constructor
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+}
